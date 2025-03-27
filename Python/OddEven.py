9b19f5a6968b2feb4a2c6eec53eadead4f377af4
KavyJani <kavyjani94@gmail.com>
2025-03-27 10:20:53 +0530

    Create OddEven.py

---
+++ Python/OddEven.py
@@ -0,0 +1,7 @@
+Number= int(input("Enter a number:"))
+if Number %2==0:
+   print(f"{Number} is an even no.")
+else:
+   print (f"{Number} is an odd no.")
+if Number %4==0:
+   print (f"{Number} is multiple of 4")
